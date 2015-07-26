cbf485e8044d6a13a940ac875c49d4e168abd5cf
divsense <oleg4564@iOK.local>
2015-07-26 01:28:47 -0400

    test

--- composition/compile.sh
+++ composition/compile.sh
@@ -1,6 +1,18 @@
 #/usr/bin/env bash
 
-#cat - | jq ".main"
-cat -
+DATA=$( cat - | jq -rc "." )
 
-#docker-compose run purescript psc "${SRC}/${NAME}.purs" --output="$DEST" 
+if [ -z "$DATA" ]; then
+	echo "[COMPILE] Error: Invalid data"
+	exit 1
+fi
+
+NAME=$( echo "$DATA" | jq -rc ".name" )
+
+if [ -z "$NAME" ]; then
+	echo "[COMPILE] Error: no name spcified"
+	exit 1
+fi
+
+docker-compose run purescript psc "${ALIEN_DATA_SRC}/${NAME}.purs" --output="$ALIEN_DATA_DEST"
+
