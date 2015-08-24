9380c8a3a1396d75df59b59b69abed2d4848f635
divsense <oleg4564@iOK.local>
2015-08-24 14:36:46 -0400

    update

--- composition/compile.sh
+++ composition/compile.sh
@@ -1,4 +1,4 @@
 #/usr/bin/env bash
 
-docker-compose run --rm purescript psc "${ALIEN_INPUT}" --output="${ALIEN_OUTPUT}"
+docker-compose run --rm purescript psc "${ALIEN_INPUT}/${CMD_PARAM}" --output="${ALIEN_OUTPUT}/out"
 
