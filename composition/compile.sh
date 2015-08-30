79d843f9ef50524e33378e34c39b8d485541b91a
divsense <oleg4564@iOK.local>
2015-08-30 01:55:15 -0400

    compile fixed

--- composition/compile.sh
+++ composition/compile.sh
@@ -1,4 +1,4 @@
 #/usr/bin/env bash
 
-docker run --rm -w "${ALIEN_INPUT}" --volumes-from=initialcomposition_exchangevolume_1 divsense_purescript pulp build -O -o "${ALIEN_OUTPUT}" -t out
+docker run --rm -w "${ALIEN_INPUT}" --volumes-from=initialcomposition_exchangevolume_1 divsense_purescript pulp build -O -t "${ALIEN_OUTPUT}/out"
 
